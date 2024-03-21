0f8ce14a290a27acd78a61faafbe984cebd712d4
Victory Amadi <lavictoire633@gmail.com>
2024-03-21 19:11:38 +0100

    Tasks 0 - 2 of the Flask project, starting up a Flask application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module starts up a Flask application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Displays a custom text"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays another custom text"""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def cee(value):
+    """Displays a dynamic text beginning with C"""
+    return "C {}".format(value.replace("_", " "))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
