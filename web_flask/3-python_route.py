36587b35b754bbefd60c404fa1a4302c2d4d308c
Victory Amadi <lavictoire633@gmail.com>
2024-03-23 14:50:18 +0100

    Files for the web flask project

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -20,5 +20,11 @@ def cee(text):
     """Displays a dynamic text beginning with C"""
     return "C {}".format(text.replace("_", " "))
 
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """Displays a dynamic text beginning with Python"""
+    return "Python {}".format(text.replace("_", " "))
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -20,5 +20,17 @@ def cee(text):
     """Displays a dynamic text beginning with C"""
     return "C {}".format(text.replace("_", " "))
 
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """Displays a dynamic text beginning with Python"""
+    return "Python {}".format(text.replace("_", " "))
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_text(n):
+    """Displays a dynamic text only if n is a number"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
